008c3974d5787d060e00fe55f8c5e63a1718f722
João Ricardo Arcoverde <92701893+Joao-Ricardo-Arcoverde@users.noreply.github.com>
2024-10-15 20:51:10 -0300

    Update 2_Europa.py

--- 2_Europa.py
+++ 2_Europa.py
@@ -3,6 +3,9 @@ import plotly.express as px
 import json
 from Mundo import *
 
+europa = mundo[mundo["continente"]=="Europe"]
+europa_populosos = europa[europa["ano"]==2007].sort_values("pop", ascending=False)
+
 st.set_page_config(page_title="Europa", page_icon="🇩🇪", layout="wide")
 
 st.subheader("Europa")
@@ -18,19 +21,33 @@ st.markdown("""
 
 st.divider()
 
+fig13 = px.bar(
+        europa_populosos,
+        x="país",
+        y="pop",
+        color="pop",
+        color_continuous_scale="blues",
+        text_auto=True
+    ).update_layout(title="Populações Europeias",
+                        xaxis_title="Países",
+                        yaxis_title="População",
+                        legend_title="População",
+                    template="plotly_white",hovermode="x").update_traces(hovertemplate=None)
+
+fig14 = px.scatter(europa[europa["ano"]==2007],
+                                 x="PIBpercap",
+                                 y="ExpVida",
+                                 color="país",
+                                 size="pop",
+                                 size_max=45,
+                                 template="plotly_white",
+                                 log_x=True,
+                                 hover_data={"pop":False}
+    ).update_layout(title="Indicadores Socioeconômicos Europeus",
+                  xaxis_title = "PIB per Capita",
+                  yaxis_title = "Expectativa de Vida").update_traces(hovertemplate=None)
+
 col1, col2 = st.columns(2)
 
-col1.plotly_chart(grafico_barras_populosos("Europe", "Sunset"))
-col2.plotly_chart(grafico_bolhas("Europe"))
-
-mais_populoso, maior_expectativa_vida, maior_pib_per_capita, menos_populoso,menor_expectativa_vida, menor_pib_per_capita = calcular_metricas_continente("Europe")
-
-col3, col4, col5 = st.columns(3)
-col6, col7, col8 = st.columns(3)
-
-col3.metric(label="Mais Populoso", value=f"{mais_populoso}")
-col4.metric(label="Maior Expectativa de Vida", value=f"{maior_expectativa_vida}")
-col5.metric(label="Maior PIB per Capita", value=f"{maior_pib_per_capita}")
-col6.metric(label="Menos Populoso", value=f"{menos_populoso}")
-col7.metric(label="Menor Expectativa de Vida", value=f"{menor_expectativa_vida}")
-col8.metric(label="Menor PIB per Capita", value=f"{menor_pib_per_capita}")
+col1.plotly_chart(fig13)
+col2.plotly_chart(fig14)
